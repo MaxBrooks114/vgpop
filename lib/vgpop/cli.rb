f6303923c8cc1f13a1e2fb20736b201ccab0efb0
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-03-01 17:19:10 -0500

    changing CLI methods

--- lib/vgpop/cli.rb
+++ lib/vgpop/cli.rb
@@ -8,6 +8,9 @@ class Vgpop::CLI
   def list_vgs
     puts "Today's most popular video games:"
     @games = Vgpop::Game.popular
+    @games.each.with_index(1) do |game, i|
+      puts "#{i}. #{game.name}"
+    end
   end
 
    def menu
@@ -15,27 +18,32 @@ class Vgpop::CLI
      while input!= "exit"
        puts "You can enter the number of the game you are interested in to see information about it."
        puts "To see games by console enter the console you're interested in."
-       puts "To see games by genre enter  the genre you're interested in."
+       puts "To see games by genre enter the genre you're interested in."
        puts "To see the game with the best reviews enter best game."
        puts "To see the game with the worst reviews enter worst game"
        puts "Otherwise enter exit to leave"
         input = gets.strip.downcase
-        case input
-        when "1"
-          puts "more info on videogame 1"
-        when "2"
-          puts "more info on videogame 2"
-        when "list"
-          list_vgs
-        when "xbox one"
+        if input.to_i > 0
+         game = @games[input.to_i- 1]
+           puts "#{game.name}"
+                "#{game.console}"
+                "#{game.genre}"
+                "#{game.score}"
+                "#{game.reviews}"
+                "#{game.price}"
+                "#{game.url}"
+                "#{game.desc}"
+        elsif input== "xbox one"
           puts "games on xbox one"
-        when "action"
+        elsif input== "action"
           puts "action games"
-        when "best game"
+        elsif input== "best game"
           puts "game with highest reviews"
-        when "worst game"
+        elsif input== "worst game"
           puts "game with worst reviews"
-        when "exit"
+        elsif input == "list"
+          list_vgs
+        elsif input == "exit"
           goodbye
         else
           puts "what you talkin bout bro"
