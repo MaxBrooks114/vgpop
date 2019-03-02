258f66457471842b1388cf76be91044cec02623f
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-03-02 10:03:12 -0500

    Modified scrape method arrays

--- lib/vgpop/game.rb
+++ lib/vgpop/game.rb
@@ -2,35 +2,33 @@ class Vgpop::Game
 
    attr_accessor :name, :console, :genre, :score, :reviews, :price, :url, :desc
 
-  def self.popular
-    #scrape gamerankings.com to return 10 most popular games
-    self.scrape_games
+
+  @@games=[]
+
+  def initialize(name)
+    @name=name
+    @@games << self
   end
 
-
-  def self.scrape_games
-    games=[]
-    games << self.scrape_gamerankings
+  def self.games
+    @@games
   end
+
 
   def self.scrape_gamerankings
     doc= Nokogiri::HTML(open("https://www.gamerankings.com/"))
-    games= doc.search("tr").text.split("\r\n\t\t")
+    games= doc.search("tr").text.strip.split("\r\n\t\t")
     games.map!{|game| game.gsub("\t","")}.delete("")
     popular_games = games[0,21]
-    popular_games_names_consoles = popular_games.map{|game|
-                               if game.index(". ") != nil
-                                game[3..game.length]
-                               end}.compact
-    popular_games_names = popular_games_names_consoles.map{|game| game.sub /\s*\(.+\)$/, ''}
+    popular_g_n_c = popular_games.map{|game| if game.index(". ") != nil
+                                                 game[3..game.length]
+                                               end}.compact
+    popular_games_names = popular_g_n_c.map{|game| game.strip.sub /\s*\(.+\)$/, ''}
 
-
-
-    game= self.new
-    game.name=
     binding.pry
-
-
   end
+
+
+
 
 end
