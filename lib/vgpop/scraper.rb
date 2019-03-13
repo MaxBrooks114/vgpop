2a2ed52be646a9bcc412235c78c89d46d6472437
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-03-13 14:47:29 -0400

    Update scraper.rb

--- lib/vgpop/scraper.rb
+++ lib/vgpop/scraper.rb
@@ -9,6 +9,7 @@ class Vgpop::Scraper
           game.console = tr.text.scan(/\(([^\)]+)\)/).last.first
           game.score = tr.text.scan(/(\d\S*%)/).last.join
           game.url = "https://www.gamerankings.com#{tr.search("a").attr("href").text}"
+          game.purchase_url = "https://www.gamestop.com/browse?nav=16k-3-#{game.name},28zu0"
           game.save
       end
     end
