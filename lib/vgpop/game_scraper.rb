61e015cb25047914fcf0f6945607a16b03422a81
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-03-14 16:36:58 -0400

    changed genre scraper to work in learn IDE

--- lib/vgpop/game_scraper.rb
+++ lib/vgpop/game_scraper.rb
@@ -5,7 +5,7 @@ class Vgpop::Gamescraper
      game = Vgpop::Game.games[input.to_i- 1]
      game.desc = @doc.search("div.details").first.text.chop.gsub(/^\s*/, '').strip
      game.reviews = @doc.search("td").search("a").first.text.scan(/\d/).join
-     game.genre = @doc.search("#content").search(".crumbs").text.strip.split("\r\n » ")[1..].join(", ")
+     game.genre = @doc.search("#content").search(".crumbs").text.strip.split("\r\n » ").drop(1).join(", ")
      game.release_date = @doc.search("body").search("b").last.text.scan(/:\s*\K.*/).join
    end
 
